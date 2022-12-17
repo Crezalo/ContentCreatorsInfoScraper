ad1f9407863a694eca2de8f9f6276630d2ec72c7
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2022-12-17 22:38:59 +0530

    Use Second_cut_explored_channels to getchannelInfo

--- youtubeGetChannelInfoForChannelNames.py
+++ youtubeGetChannelInfoForChannelNames.py
@@ -156,7 +156,7 @@ class channelInfoFromName:
             return []
 
 with open(
-    "ExploredChannelsDistinct.tsv",
+    "Second_cut_explored_channels.tsv",
     "r",
     encoding="utf-8",
 ) as file:
